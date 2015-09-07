c115fcf8a4f08ec637be9a581c56054108f01c69
masakyx <weavermasaki@gmail.com>
2015-09-08 02:29:17 +0900

    デザインほぼ終了

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -4,36 +4,74 @@ angular.module('starter.controllers', ['ionic','ionic.contrib.frostedGlass'])
 //--DashCtrl------
 //----------------
 .controller('DashCtrl', function($scope,TennisID,socket) {
-     var creater = $("#creater"),
-        player1 = $("#player1"),
-        player2 = $("#player2");
     var id;
     var tennisdata = TennisID.all();
     
     $scope.setChoice = "1";
     $scope.gameChoice = "6";
+    $scope.gametype =  "1";
+    $scope.agtiebreak = true;
+    $scope.agdeuce = true;
+    $scope.player1 = "Player1";
+    $scope.player2 = "Player2";
+
+    $scope.choseType = function(){
+      if($scope.gametype == "1"){
+        $scope.daubles = false;
+        $scope.player1 = "Player1";
+        $scope.player2 = "Player2";
+      }else{
+        $scope.daubles = true;
+        $scope.player1 = "フォアサイドPlayer1";
+        $scope.player2 = "バックサイドplayer2";
+        $scope.player3 = "バックサイドPlayer1";
+        $scope.player4 = "フォアサイドplayer2";
+      }
+    }
 
     $scope.gamestartclick = function(){
-      if(creater.val() == "" || player1.val()=="" || player2.val() == ""){
-          window.alert("名前を入力してください");
+      var creater = $scope.agcreater,
+          player1 = $scope.agplayer1,
+          player2 = $scope.agplayer2,
+          player3 = $scope.agplayer3,
+          player4 = $scope.agplayer4,
+          set = $scope.setChoice,
+          game = $scope.gameChoice,
+          tiebreak = $scope.agtiebreak,
+          deuce = $scope.agdeuce;
+      if($scope.gametype == "1"){
+        if(creater == null || player1== null || player2== null){
+            window.alert("名前を入力してください");
+        }else{
+          if(confirm("試合データを作成しますか？")){
+              tennisdata.creater = creater;
+              tennisdata.player1 = player1;
+              tennisdata.player2 = player2;
+              tennisdata.gametype = "1";
+              tennisdata.set = set;
+              tennisdata.game = game;
+              tennisdata.tiebreak = tiebreak;
+              tennisdata.deuce = deuce;
+              socket.emit("tennis-start",{tennis:tennisdata});
+            }
+          }
       }else{
-        if(confirm("試合データを作成しますか？")){
-          tennisdata.creater = creater.val();
-          tennisdata.player1 = player1.val();
-          tennisdata.player2 = player2.val();
-          tennisdata.set = $("input[name='setradio']:checked").val();
-          tennisdata.game = $("input[name='gameradio']:checked").val();
-          if($("#tiebreak:checked").val()){
-            tennisdata.tiebreak = true;
-          }else{
-            tennisdata.tiebreak = false;
+        if(creater == null || player1== null || player2== null || player3==null ||player4 == null){
+            window.alert("名前を入力してください");
+        }else{
+          if(confirm("試合データを作成しますか？")){
+              tennisdata.creater = creater;
+              tennisdata.player1 = player1;
+              tennisdata.player2 = player2;
+              tennisdata.player3 = player3;
+              tennisdata.player4 = player4;
+              tennisdata.gametype = "2";
+              tennisdata.set = set;
+              tennisdata.game = game;
+              tennisdata.tiebreak = tiebreak;
+              tennisdata.deuce = deuce;
+              socket.emit("tennis-start",{tennis:tennisdata});
           }
-          if($("#deuce:checked").val()){
-            tennisdata.deuce = true;
-          }else{
-            tennisdata.deuce = false;
-          }
-          socket.emit("tennis-start",{tennis:tennisdata});
         }
       }
     }
@@ -46,32 +84,141 @@ angular.module('starter.controllers', ['ionic','ionic.contrib.frostedGlass'])
 })
 
 //----------------
-//--scoreboardCtrl------
-//----------------
+//--scoreboardCtrl----------------------------------------------------------------
+//----------------------
 .controller('scoreboardCtrl',function($scope,TennisID){
+//---------変数設定--------------------------------------------------------------
     var set1=0,set2=0,game1=0,game2=0,point1=0,point2=0;
-    $scope.agcreater = TennisID.all().creater;
-    $scope.agplayer1 = TennisID.all().player1;
-    $scope.agplayer2 = TennisID.all().player2;
+    var player1=TennisID.all().player1,
+        player2=TennisID.all().player2,
+        player3=TennisID.all().player3,
+        player4=TennisID.all().player4,
+        creater=TennisID.all().creater,
+        gametype=TennisID.all().gametype;
+
+    var Nserverchange = 0,
+        Nreceiverchange = 0;
+
+
+//---------初期設定----------------------------------------------------------
+    $scope.agcreater = creater;
+    $scope.agserver1 = TennisID.all().player1;
+    $scope.agrecever1 = TennisID.all().player1;
+    $scope.agserver2 = TennisID.all().player2;
+    $scope.agrecever2 = TennisID.all().player2;
+    $scope.checkserver1 = true;
+    if(gametype == "1"){
+      $scope.agplayer1 = player1;
+      $scope.agplayer2 = player2;
+    }else{
+      $scope.agplayer1 = player1+" & "+player3;
+      $scope.agplayer2 = player2+" & "+player4;
+    }
     $scope.agset1 = set1;
     $scope.aggame1 = game1;
     $scope.agpoint1 = point1;
     $scope.agset2 = set2;
     $scope.aggame2 = game2;
     $scope.agpoint2 = point2;
-
-    $scope.click = function(){
-      set1++;
-      set2 = set2 +2;
-      console.log(set1+"/"+set2);
-      $scope.agset1 = set1;
-      $scope.aggame1 = game1;
-      $scope.agpoint1 = point1;
-      $scope.agset2 = set2;
-      $scope.aggame2 = game2;
-      $scope.agpoint2 = point2;
+    $scope.serverbutton1 = true;
+//-------player1ボタンクリック挙動------------------------------------------------
+$scope.servicein1 = function(){  
+}
+$scope.serviceace1 = function(){  
+}
+$scope.fault1 = function(){  
+}
+$scope.returnin1 = function(){  
+}
+$scope.returnace1 = function(){  
+}
+$scope.returnmiss1 = function(){  
+}
+$scope.win1 = function(){  
+}
+$scope.side1 = function(){  
+}
+$scope.back1 = function(){  
+}
+$scope.net1 = function(){  
+}
+//-------player2ボタンクリック挙動------------------------------------------------
+$scope.servicein2 = function(){  
+}
+$scope.serviceace2 = function(){  
+}
+$scope.fault2 = function(){  
+}
+$scope.returnin2 = function(){  
+}
+$scope.returnace2 = function(){  
+}
+$scope.returnmiss2 = function(){  
+}
+$scope.win2 = function(){  
+}
+$scope.side2 = function(){  
+}
+$scope.back2 = function(){  
+}
+$scope.net2 = function(){  
+}
+//--------チェンジボタンクリック挙動-----------------------------------------------------
+$scope.serverchange = function(){
+   ServerChange();
+}
+$scope.courtchange = function(){
+}
+$scope.pointback = function(){
+}
+$scope.finishgame = function(){
+}
+//---------関数------------------------------------------------------------------
+  function ServerChange(){
+    Nserverchange++;
+    Nreceiverchange++;
+    if(gametype == "1"){
+      if(Nserverchange == 1){
+        $scope.checkserver1 = false;
+        $scope.checkserver2 = true;
+        $scope.serverbutton1 = false;
+        $scope.serverbutton2 = true;
+      }else if(Nserverchange == 2){
+        $scope.checkserver2 = false;
+        $scope.checkserver1 = true;
+        $scope.serverbutton2 = false;
+        $scope.serverbutton1 = true;
+        Nserverchange=0;
+      }
+    }else{
+      if(Nserverchange == 1){
+        $scope.agserver2 = player2;
+        $scope.checkserver1 = false;
+        $scope.checkserver2 = true;
+        $scope.serverbutton1 = false;
+        $scope.serverbutton2 = true;
+      }else if(Nserverchange == 2){
+        $scope.agserver1 = player3;
+        $scope.checkserver2 = false;
+        $scope.checkserver1 = true;
+        $scope.serverbutton2 = false;
+        $scope.serverbutton1 = true;
+      }else if(Nserverchange == 3){
+        $scope.agserver2 = player4;
+        $scope.checkserver1 = false;
+        $scope.checkserver2 = true;
+        $scope.serverbutton1 = false;
+        $scope.serverbutton2 = true;
+      }else if(Nserverchange == 4){
+        $scope.agserver1 = player1;
+        $scope.checkserver2 = false;
+        $scope.checkserver1 = true;
+        $scope.serverbutton2 = false;
+        $scope.serverbutton1 = true;
+        Nserverchange=0;
+      }
     }
-
+  }
 })
 
 //---view game in real time controller-----------------------------------------------------
@@ -154,4 +301,7 @@ angular.module('starter.controllers', ['ionic','ionic.contrib.frostedGlass'])
   $scope.settings = {
     enableFriends: true
   };
+})
+
+.controller('not',function($scope){
 });
